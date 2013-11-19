0fc62dc0ecb00ecc600e457630d22c028dd09b6f
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2013-11-19 23:01:49 +0100

    rinetd startup scripts

--- php/5.5/start.sh
+++ php/5.5/start.sh
@@ -2,10 +2,12 @@
 
 # update IP redirecting
 # 127.0.0.1:3306 -> DB:3306
-cp -f /etc/rinetd.conf.tpl /etc/rinetd.conf
-sed -i -e "s/%{DB_TCP_ADDR}/$DB_PORT_3306_TCP_ADDR/" /etc/rinetd.conf
-sed -i -e "s/%{DB_TCP_PORT}/$DB_PORT_3306_TCP_PORT/" /etc/rinetd.conf
-/etc/init.d/rinetd restart
+if [[ ! -n $DB_PORT_3306_TCP_ADDR && ! -n $DB_PORT_3306_TCP_PORT ]]; then
+  echo "127.0.0.1 3306 $DB_PORT_3306_TCP_ADDR $DB_PORT_3306_TCP_PORT" >> /etc/rinetd.conf
+  /etc/init.d/rinetd restart
+else
+  /etc/init.d/rinetd stop
+fi
 
-# start php fpm
+
 php5-fpm
